c1757b8df5ebe632c0b192611bcd6c56a193d318
Arnarlulu <arnar.s.reiten@gmail.com>
2017-11-01 14:05:27 +0100

    **ARNAR** - ny java-class Bconnect og oppdatert APK-innstillinger + minSDK er nå 23

--- app/src/main/java/com/example/baard/mysqldemo/Bconnect.java
+++ app/src/main/java/com/example/baard/mysqldemo/Bconnect.java
@@ -10,6 +10,7 @@ import com.empatica.empalink.delegate.EmpaDataDelegate;
 import com.empatica.empalink.delegate.EmpaStatusDelegate;
 
 import android.Manifest;
+import android.view.View;
 import android.app.ActionBar;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -22,7 +23,9 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.empatica.empalink.ConnectionNotAllowedException;
@@ -40,13 +43,62 @@ import static com.empatica.empalink.config.EmpaStatus.*;
  */
 
 public abstract class Bconnect extends AppCompatActivity implements EmpaDataDelegate, EmpaStatusDelegate{
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION=2;
+    private static final long STREAMING_TIME = 3600000; // Stops streaming 3600 seconds(1 hour) after connection
+    private static final long SCANNING_TIME = 10000;//Stop scanning after 10 sec
+
+    private static final String EMPATICA_API_KEY = "234acf07689e4d2aacfe46bf5b6a816c";
+
     private EmpaDeviceManager deviceManager;
+    private EmpaStatus status;
+
+    private TextView accel_xLabel;
+    private TextView accel_yLabel;
+    private TextView accel_zLabel;
+    private TextView bvpLabel;
+    private TextView edaLabel;
+    private TextView ibiLabel;
+    private TextView hrLabel;
+    private TextView temperatureLabel;
+    private TextView batteryLabel;
+    private TextView statusLabel;
+    private Button searchDeviceBtn;
+    private TextView deviceNameLabel;
+    private RelativeLayout dataCnt;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        deviceManager = new EmpaDeviceManager(getApplicationContext(), this, this);
-        deviceManager.authenticateWithAPIKey("234acf07689e4d2aacfe46bf5b6a816c");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bconnect);
 
+
+
+       /* statusLabel = (TextView) findViewById(R.id.status);
+        dataCnt = (RelativeLayout) findViewById(R.id.dataxArea);
+        accel_xLabel = (TextView) findViewById(R.id.accel_x);
+        accel_yLabel = (TextView) findViewById(R.id.accel_y);
+        accel_zLabel = (TextView) findViewById(R.id.accel_z);
+        bvpLabel = (TextView) findViewById(R.id.bvp);
+        edaLabel = (TextView) findViewById(R.id.eda);
+        ibiLabel = (TextView) findViewById(R.id.ibi);
+        hrLabel = (TextView) findViewById(R.id.hr);
+        temperatureLabel = (TextView) findViewById(R.id.temperature);
+        batteryLabel = (TextView) findViewById(R.id.battery);
+        deviceNameLabel = (TextView) findViewById(R.id.deviceName);
+        searchDeviceBtn=(Button) findViewById(R.id.search_device);
+        searchDeviceBtn.setEnabled(false);*/
+
+       //Lager en ny EmpaDeviceManager
+        deviceManager = new EmpaDeviceManager(getApplicationContext(), this, this);
+        deviceManager.authenticateWithAPIKey(EMPATICA_API_KEY);
+        requestPermissions(new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        }, PERMISSION_REQUEST_COARSE_LOCATION);
 
     }
 }
