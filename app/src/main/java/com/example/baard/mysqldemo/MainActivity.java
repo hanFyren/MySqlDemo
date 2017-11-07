574b908db55c1851bfa26a386e7adf8623a86bee
Arnarlulu <arnar.s.reiten@gmail.com>
2017-11-07 17:40:03 +0100

    javaklasse bluetooth lagt til *2

--- app/src/main/java/com/example/baard/mysqldemo/MainActivity.java
+++ app/src/main/java/com/example/baard/mysqldemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -60,5 +61,11 @@ public class MainActivity extends AppCompatActivity {
 //##### Funksjonen kalles fra kanpp, kodet i XML
         //Starter ny aktivitet, Registrer
        startActivity(new Intent(this, Registrer.class));
+    }
+
+    public void ConnectE4(View view){
+        Log.i("**********", " TRYKKET KNAPP CONNECT *************");
+        Intent intent = new Intent(this, bluetooth.class);
+        startActivity(intent);
     }
 }
